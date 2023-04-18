843cbd161a01c3e8311876741f3658abefa991c4
shayyo <shay.yosef@gmail.com>
2023-04-18 11:28:52 +0300

    Update main.tf

--- app/main.tf
+++ app/main.tf
@@ -1,1 +1,7 @@
-#
+resource "aws_instance" "cndr_ec2" {
+
+  ami 		= var.imageName
+  instance_type = var.instanceType
+  count		= var.instanceCount
+  key_name	= var.keyName
+  associate_public_ip_address = true
